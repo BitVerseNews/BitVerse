b564dc25fc8820a0198bf772dcac4b4b6b50179d
John Doe <john@doe.org>
2022-02-17 14:23:00 -0500

    init Project with routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import MainPage from './pages/mainPage/MainPage';
+import ArticlePage from './pages/articlePage/ArticlePage';
+import Navbar from './pages/navbar/Navbar';
+import Error from './pages/Error/Error';
 import './App.css';
 
 function App() {
   return (
-   <>
-   </>
+   <Router>
+     <Navbar />
+     <Routes>
+       <Route path="/" element={<MainPage/>}/> 
+        <Route path="/ArticlePage" element={<ArticlePage/>}/> 
+        {/*This needs to be the last route*/ }
+        <Route path="*" element={<Error/>} />
+       </Routes>
+       
+      
+       
+
+   </Router>
   );
 }
 
