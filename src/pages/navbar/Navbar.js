b042312928dc1590f31ea22bc7cd797ebd85b491
John Doe <john@doe.org>
2022-02-18 12:54:26 -0500

    Navbar

--- src/pages/navbar/Navbar.js
+++ src/pages/navbar/Navbar.js
@@ -12,7 +12,7 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Drama', 'Merch', 'News'];
+const pages = ['Drama', 'Merch', 'News','Gaming'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 const Navbar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
